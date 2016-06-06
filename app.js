6df4b0b7d5dd2f443e7b4161fb08b810a3bda646
Eros Espínola González <eros.espinola.gonzalez@gmail.com>
2016-06-06 00:54:12 -0500

    services

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/user');
+var routes = require('./routes/routes');
 
 var app = express();
 
@@ -32,7 +31,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/routes.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+	var langFile = 'es.mx.json';
+	fs.readFile('lang/' + langFile, (err, data) => {
+		if (err) 
+			throw err;
+		res.render('index', { lang: JSON.parse(data) });
+	});	
+});
+
+/* GET services page. */
+router.get('/services', function(req, res) {
+	var langFile = 'es.mx.json';
+	fs.readFile('lang/' + langFile, (err, data) => {
+		if (err) 
+			throw err;
+		res.render('services', { lang: JSON.parse(data) });
+	});	
+});
+
+module.exports = router;
