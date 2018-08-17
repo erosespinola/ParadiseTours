973b7e3b8d233bcffb6440cc076e4f908ffa5e14
Eros Espínola González <eros.espinola@Eross-MacBook-Pro.local>
2018-08-17 14:22:09 +0700

    Fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,13 @@
 
 $(document).ready(function () {
-	$("#lightgallery").lightGallery(); 
+  $("#lightgallery").lightGallery();
 });
-
 window.initMap = function() {
   var myLatLng = { lat: 20.6544684, lng: -105.2320376 };
-
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
     center: myLatLng
   });
-
   var marker = new google.maps.Marker({
     position: myLatLng,
     map: map,
@@ -19,25 +16,72 @@ window.initMap = function() {
 };
 
 window.send = function() {
-  var email = $('#email');
   var name = $('#name');
   var subject = $('#subject');
   var message = $('#message');
-
-  
-  $("#email-group").removeClass("has-error");
+  var email = $("#email");
+  var hasError = false;
   $("#name-group").removeClass("has-error");
   $("#subject-group").removeClass("has-error");
   $("#message-group").removeClass("has-error");
-    
-  if (!email.val())
+  $("#email-group").removeClass("has-error");
+  if (!email.val()) {
     $("#email-group").addClass("has-error");
-  if (!name.val())
+    hasError = true;
+  }
+  if (!name.val()) {
     $("#name-group").addClass("has-error");
-  if (!subject.val())
+    hasError = true;
+  }
+  if (!subject.val()) {
     $("#subject-group").addClass("has-error");
-  if (!message.val())
+    hasError = true;
+  }
+  if (!message.val()) {
     $("#message-group").addClass("has-error");
+    hasError = true;
+  }
+  if (!hasError) {
+    var text = 'Nombre: ' + name.val() + ', Email: ' + email.val() + ', Mensaje: ' + message.val();
+    var request = $.post( "http://paradisetourstransportations.com/email", {
+      subject: subject.val(),
+      email: email.val(),
+      text: text
+    });
+    var lang = 'es';
+    var query = window.location.search.split('lang=');
+    if (query.length === 2) {
+      lang = query[1];
+    }
+    request.done(function() {
+      if (lang === 'es') {
+        alert('Su mensaje ha sido envido. Pronto nos pondremos en contacto con usted.');
+      } else if (lang === 'en') {
+        alert('Your message has been sent. We will contact you soon.');
+      } else {
+        alert('Su mensaje ha sido envido. Pronto nos pondremos en contacto con usted.');
+      }
+    })
+    .fail(function() {
+      if (lang === 'es') {
+        alert('Su mensaje no pudo ser envido. Por favor contáctenos por teléfono.');
+      } else if (lang === 'en') {
+        alert('Your message could not been sent. Please contact us by phone.');
+      } else {
+        alert('Su mensaje no pudo ser envido. Por favor contáctenos por teléfono.');
+      }
+    })
+    .always(function() {
+      name.val('');
+      email.val('');
+      message.val('');
+      subject.val('');
+      $("#name-group").removeClass("has-error");
+      $("#subject-group").removeClass("has-error");
+      $("#message-group").removeClass("has-error");
+      $("#email-group").removeClass("has-error");
+    });
+  }
 };
 
 localStorage['lang'] = 'es';
--- routes/routes.js
+++ routes/routes.js
@@ -1,66 +1,95 @@
 var fs = require('fs');
+var nodemailer = require('nodemailer');
 var express = require('express');
 var router = express.Router();
+var langRoute = './lang/';
 
 router.get('/', function(req, res) {
-	var lang = req.query.lang;
-	if (lang !== 'en' && lang !== 'es')
-		lang = 'es';
-	var langFile = lang + '.json';
-	fs.readFile('lang/' + langFile, function(err, data) {
-		if (err) 
-			throw err;
-		res.render('index', { lang: JSON.parse(data) });
-	});	
+  var lang = req.query.lang;
+  if (lang !== 'en' && lang !== 'es')
+    lang = 'es';
+  var langFile = lang + '.json';
+  fs.readFile(langRoute + langFile, function(err, data) {
+    if (err)
+      throw err;
+    res.render('index', { lang: JSON.parse(data) });
+  });
 });
 
-router.get('/services/', function(req, res) {
-	var lang = req.query.lang;
-	if (lang !== 'en' && lang !== 'es')
-		lang = 'es';
-	var langFile = lang + '.json';
-	fs.readFile('lang/' + langFile, function(err, data) {
-		if (err) 
-			throw err;
-		res.render('services', { lang: JSON.parse(data) });
-	});	
+router.post('/email', function(req, res) {
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'paradisetourstransport.contact@gmail.com',
+      pass: 'paradise1234pass'
+    }
+  });
+  var mailOptions = {
+    from: req.body.email,
+		to: 'dspa29@hotmail.com',
+		subject: req.body.subject,
+		text: req.body.text
+	};
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      res.status(500).json({error: error});
+    } else {
+      res.status(200).json({});
+    };
+  });
 });
 
-router.get('/gallery/', function(req, res) {
-	var lang = req.query.lang;
-	if (lang !== 'en' && lang !== 'es')
-		lang = 'es';
-	var langFile = lang + '.json';
-	fs.readFile('lang/' + langFile, function(err, data) {
-		if (err) 
-			throw err;
-		res.render('gallery', { lang: JSON.parse(data) });
-	});	
+router.get('/services', function(req, res) {
+  var lang = req.query.lang;
+  if (lang !== 'en' && lang !== 'es')
+    lang = 'es';
+  var langFile = lang + '.json';
+  fs.readFile(langRoute + langFile, function(err, data) {
+    if (err)
+      throw err;
+    res.render('services', { lang: JSON.parse(data) });
+  });
 });
 
-router.get('/about/', function(req, res) {
-	var lang = req.query.lang;
-	if (lang !== 'en' && lang !== 'es')
-		lang = 'es';
-	var langFile = lang + '.json';
-	fs.readFile('lang/' + langFile, function(err, data) {
-		if (err) 
-			throw err;
-		res.render('about', { lang: JSON.parse(data) });
-	});	
+router.get('/gallery', function(req, res) {
+  var lang = req.query.lang;
+  if (lang !== 'en' && lang !== 'es')
+    lang = 'es';
+  var langFile = lang + '.json';
+  fs.readFile(langRoute + langFile, function(err, data) {
+    if (err)
+      throw err;
+    res.render('gallery', { lang: JSON.parse(data) });
+  });
 });
 
-router.get('/contact/', function(req, res) {
-	var lang = req.query.lang;
-	if (lang !== 'en' && lang !== 'es')
-		lang = 'es';
-	var langFile = lang + '.json';
-	fs.readFile('lang/' + langFile, function(err, data) {
-		if (err) 
-			throw err;
-		res.render('contact', { lang: JSON.parse(data) });
-	});	
+router.get('/about', function(req, res) {
+  var lang = req.query.lang;
+  if (lang !== 'en' && lang !== 'es')
+    lang = 'es';
+  var langFile = lang + '.json';
+  fs.readFile(langRoute + langFile, function(err, data) {
+    if (err)
+      throw err;
+    res.render('about', { lang: JSON.parse(data) });
+  });
 });
 
+router.get('/contact', function(req, res) {
+  var lang = req.query.lang;
+  if (lang !== 'en' && lang !== 'es')
+    lang = 'es';
+  var langFile = lang + '.json';
+  fs.readFile(langRoute + langFile, function(err, data) {
+    if (err)
+      throw err;
+    res.render('contact', { lang: JSON.parse(data) });
+  });
+});
+
+router.get('/googled5aa149259436c16.html', function(req, res){
+	res.render('google');
+});
 
 module.exports = router;
